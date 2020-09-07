6da4ff5b3a414ac759e005cac1470ccf8ee4711a
kashifp <kashif.peshimam9@gmail.com>
2020-09-06 22:35:25 -0400

    Updates

---
+++ precache-manifest.3f44d20ced26d95f1491fffec60ba9a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aef067a114612ab816bb883b2f6a27cb",
+    "url": "/activity-tracker/index.html"
+  },
+  {
+    "revision": "2e97ff5d2dcc5ed5bf15",
+    "url": "/activity-tracker/static/js/2.c742aa88.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/activity-tracker/static/js/2.c742aa88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f5767e751ce3e22a5ca",
+    "url": "/activity-tracker/static/js/main.b16c580a.chunk.js"
+  },
+  {
+    "revision": "1fe942e1af477f213d37",
+    "url": "/activity-tracker/static/js/runtime-main.74c0b2e6.js"
+  }
+]);
